9e791e1c7ad57fd0992c70f8e89c69069fcd978b
monomadic <email@server.com>
2022-02-06 22:54:00 +0800

    read inner chunk data with tests

--- src/ni_container.rs
+++ src/ni_container.rs
@@ -10,24 +10,47 @@ pub fn read(buf: &[u8]) -> Result<HeaderChunk, Error> {
 
 #[derive(BinRead, Debug)]
 pub struct HeaderChunk {
-    pub length: u32,
-    pub unknown_a: u32, // always 0
-    pub unknown_b: u32,
+    pub length: u64,
+    pub unknown_a: u32, // always 1
     //#[br(magic = b"DSIN", assert(id==108))]
-    pub tag: [char;4],
-    pub id: u32,
-    pub unknown_c: u32,
-    pub checksum: [u32;4], // still unknown, maybe md4?
+    pub tag: [char; 4],
+    pub id: u64,
+    pub checksum: [u8;16], // still unknown, maybe md4?
+    pub data_len: u32,
+    #[br(count = data_len - 4)]
+    pub data_chunk: Vec<u8>,
+    pub unknown_b: u32, // always 1?
+    pub children: u64,
+    pub inner_tag: [char; 4],
+    pub inner_id: u32,
+    pub inner_length: u64,
+    #[br(count = inner_length - 8)]
+    pub inner_chunk: Vec<u8>,
 }
 
 impl HeaderChunk {
     pub fn tag(&self) -> String {
         self.tag.iter().collect::<String>()
     }
+    pub fn checksum(&self) -> String {
+        self.checksum
+            .iter()
+            .map(|h| format!("{:02x}", h))
+            .collect::<String>()
+    }
 }
 
 impl ToString for HeaderChunk {
-    fn to_string(&self) -> std::string::String { 
-        format!("<{}>", &self.tag())
+    fn to_string(&self) -> std::string::String {
+        format!("<{}></{}>", &self.tag(), &self.tag())
     }
+}
+
+#[derive(BinRead, Debug)]
+struct DataChunk {
+    size: u32,
+    unknown_a: u32,
+    tag: [char; 4],
+    id: u32,
+    unknown_b: u32,
 }
--- tests/ni-container-test.rs
+++ tests/ni-container-test.rs
@@ -8,9 +8,37 @@ fn test_kontakt_4_booga() {
     assert_eq!(container.unknown_b, 1);
     assert_eq!(&container.tag(), "hsin");
     assert_eq!(container.id, 1);
-    assert_eq!(container.unknown_c, 0);
+
 
     assert_eq!(container.to_string(), "<hsin>");
+
+    // assert_eq!("", format!("{:?}", container));
+}
+
+#[test]
+fn test_k5_4_demo() {
+    let file = include_bytes!("./data/k5.4-demo.nki");
+    let container = ni_file::ni_container::read(file).unwrap();
+
+    assert_eq!(container.length, 2438);
+    assert_eq!(container.unknown_a, 1);
+    assert_eq!(&container.tag(), "hsin");
+    assert_eq!(container.id, 1);
+    // assert_eq!(&container.checksum(), "444");
+    // checksums:
+    // u32le;4  0d881716e742958f4695d6978a38bca0
+    // u16le;8  17160d88958fe742d6974695bca08a38 
+    // u8;16    1617880d8f9542e797d69546a0bc388a
+
+    assert_eq!(container.data_len - 4, container.data_chunk.len() as u32);
+    assert_eq!(container.children, 1);
+    assert_eq!(container.unknown_b, 1);
+
+    assert_eq!(container.unknown_b, 1);
+
+    assert_eq!(container.to_string(), "<hsin></hsin>");
+
+    assert_eq!(container.inner_length - 8, container.inner_chunk.len() as u64);
 
     // assert_eq!("", format!("{:?}", container));
 }
