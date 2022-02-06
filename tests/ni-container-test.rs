fdee604db3b8951193f55991929decf20452e6d6
monomadic <email@server.com>
2022-02-06 22:58:01 +0800

    add test for fm8 file

--- tests/ni-container-test.rs
+++ tests/ni-container-test.rs
@@ -3,16 +3,16 @@ fn test_kontakt_4_booga() {
     let file = include_bytes!("./data/k4-booga.nki");
     let container = ni_file::ni_container::read(file).unwrap();
 
-    assert_eq!(container.length, 2440);
-    assert_eq!(container.unknown_a, 0);
+    assert_eq!(container.length, file.len() as u64);
+    assert_eq!(container.unknown_a, 1);
     assert_eq!(container.unknown_b, 1);
     assert_eq!(&container.tag(), "hsin");
     assert_eq!(container.id, 1);
 
-
-    assert_eq!(container.to_string(), "<hsin>");
-
-    // assert_eq!("", format!("{:?}", container));
+    assert_eq!(container.data_len - 4, container.data_chunk.len() as u32);
+    assert_eq!(container.children, 1);
+    assert_eq!(container.unknown_b, 1);
+    assert_eq!(container.inner_length - 8, container.inner_chunk.len() as u64);
 }
 
 #[test]
@@ -20,7 +20,7 @@ fn test_k5_4_demo() {
     let file = include_bytes!("./data/k5.4-demo.nki");
     let container = ni_file::ni_container::read(file).unwrap();
 
-    assert_eq!(container.length, 2438);
+    assert_eq!(container.length, file.len() as u64);
     assert_eq!(container.unknown_a, 1);
     assert_eq!(&container.tag(), "hsin");
     assert_eq!(container.id, 1);
@@ -33,12 +33,25 @@ fn test_k5_4_demo() {
     assert_eq!(container.data_len - 4, container.data_chunk.len() as u32);
     assert_eq!(container.children, 1);
     assert_eq!(container.unknown_b, 1);
-
-    assert_eq!(container.unknown_b, 1);
-
-    assert_eq!(container.to_string(), "<hsin></hsin>");
-
     assert_eq!(container.inner_length - 8, container.inner_chunk.len() as u64);
 
+    assert_eq!(container.to_string(), "<hsin></hsin>");
     // assert_eq!("", format!("{:?}", container));
+}
+
+#[test]
+fn test_fm8_fm7() {
+    let file = include_bytes!("./data/fm8-fm7.nfm8");
+    let container = ni_file::ni_container::read(file).unwrap();
+
+    assert_eq!(container.length, file.len() as u64);
+    assert_eq!(container.unknown_a, 1);
+    assert_eq!(container.unknown_b, 1);
+    assert_eq!(&container.tag(), "hsin");
+    assert_eq!(container.id, 1);
+
+    assert_eq!(container.data_len - 4, container.data_chunk.len() as u32);
+    assert_eq!(container.children, 1);
+    assert_eq!(container.unknown_b, 1);
+    assert_eq!(container.inner_length - 8, container.inner_chunk.len() as u64);
 }
