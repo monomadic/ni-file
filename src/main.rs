c9c8105c6d444f78382f45a87747c233207dbbdd
monomadic <email@server.com>
2022-02-15 19:28:36 +0800

    added stub for cli

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+extern crate log;
+
+pub type Error = Box<dyn std::error::Error>;
+
+fn main() {
+    // set up a logger with default level 'info'
+    env_logger::Builder::from_default_env()
+        .filter_level(log::LevelFilter::Info)
+        .init();
+
+    // match args::run() {
+    //     Ok(_) => info!("done"),
+    //     Err(e) => error!("{}", e), // todo: print error properly
+    // }
+}
