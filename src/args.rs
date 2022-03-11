4706cf1809827ce51cc42efdc93d8643b68f935d
nom <nom@nowhere>
2022-03-11 16:22:12 +0800

    cli tool has commands: extract, convert, info

--- src/args.rs
+++ src/args.rs
@@ -2,30 +2,29 @@ use std::path::PathBuf;
 use structopt::StructOpt;
 
 pub(crate) fn run() -> Result<(), Box<dyn std::error::Error>> {
-    println!("NI Extractor v0.0.1\n");
-
-    let args = Args::from_args();
-    let input = args.input;
-    let buffer = std::fs::read(input)?;
-
-    // try to detect the type of file we're dealing with
-    match crate::detect::filetype(&buffer) {
-        crate::detect::NIFileType::NIContainer => {
-            let container = ni_file::ni_container::read(&buffer)?;
-            info!("hsin id {}", container.id);
-            info!("{:?}", container);
+    let args = Command::from_args();
+    match args {
+        Command::Extract { path } => {
+            let buffer = std::fs::read(path)?;
+            crate::detect::filetype(&buffer);
         }
-        crate::detect::NIFileType::NIKontaktMonolith => todo!(),
-        crate::detect::NIFileType::KoreSound => todo!(),
-        crate::detect::NIFileType::Unknown => todo!(),
-    }
+        Command::Convert { path } => {
+            let buffer = std::fs::read(path)?;
+            crate::detect::filetype(&buffer);
+        }
+        Command::Info { path } => {
+            let buffer = std::fs::read(path)?;
+            crate::detect::filetype(&buffer);
+        }
+    };
+
     Ok(())
 }
 
 #[derive(StructOpt)]
-#[structopt(name = "extract", about = "RSDK Extractor")]
-struct Args {
-    /// Input file
-    #[structopt(parse(from_os_str))]
-    input: PathBuf,
+#[structopt(name = "ni-file", about = "NI Extractor")]
+enum Command {
+    Extract { path: PathBuf },
+    Convert { path: PathBuf },
+    Info { path: PathBuf },
 }
