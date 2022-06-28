b93bdf942e6c42b6b83d278a9bb7c173a8a06df6
nom <nom@nom.local>
2022-06-28 20:44:17 +0700

    simplify cli interface arguments

--- cli/src/args.rs
+++ cli/src/args.rs
@@ -3,36 +3,23 @@ use std::path::PathBuf;
 use structopt::StructOpt;
 
 pub(crate) fn run() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Command::from_args();
-    match args {
-        Command::Extract { path } => {
-            let buffer = std::fs::read(path)?;
-            detect::filetype(&buffer);
-        }
-        Command::Convert { path } => {
-            let buffer = std::fs::read(path)?;
-            match detect::filetype(&buffer) {
-                detect::NIFileType::NIContainer => {
-                    println!("{}", ni_file::ni_container::read(&buffer)?.to_xml());
-                }
-                detect::NIFileType::NIKontaktMonolith => todo!(),
-                detect::NIFileType::KoreSound => todo!(),
-                detect::NIFileType::Unknown => todo!(),
-            }
-        }
-        Command::Info { path } => {
-            let buffer = std::fs::read(path)?;
-            detect::filetype(&buffer);
-        }
-    };
+    let args = Args::from_args();
+    let buffer = std::fs::read(args.input)?;
 
-    Ok(())
+    match detect::filetype(&buffer) {
+        detect::NIFileType::NIContainer => {
+            println!("{}", ni_file::ni_container::read(&buffer)?.to_xml());
+            Ok(())
+        }
+        detect::NIFileType::NIKontaktMonolith => todo!(),
+        detect::NIFileType::KoreSound => todo!(),
+        detect::NIFileType::Unknown => todo!(),
+    }
 }
 
 #[derive(StructOpt)]
 #[structopt(name = "ni-file", about = "NI Extractor")]
-enum Command {
-    Extract { path: PathBuf },
-    Convert { path: PathBuf },
-    Info { path: PathBuf },
+struct Args {
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
 }
