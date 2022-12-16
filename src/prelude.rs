eaad93363ed9eea34c7df795c8034a8731cc9f24
monomadic <monomadic.crypto>
2022-12-17 00:30:32 +0800

    upgrade: env_logger

--- src/prelude.rs
+++ src/prelude.rs
@@ -3,8 +3,6 @@
 pub use crate::error::Error;
 pub use anyhow::{Context, Result};
 
-// pub type Result<T> = core::result::Result<T, Error>;
-
 // Generic Wrapper tuple struct for newtype pattern,
 // mostly for external type to type From/TryFrom conversions
 pub struct W<T>(pub T);
