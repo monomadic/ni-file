afc0a942cb35201588c9c09cbc181d15d4ca361d
monomadic <monomadic.crypto>
2023-01-06 03:13:38 +0800

    k7 test

---
+++ doc/Item.md
@@ -0,0 +1,38 @@
+# Item
+
+- afl~readItem
+- afl~read_NI
+
+## 118 RepositoryRoot
+- chunk: 58 bytes
+- @method.NI::SOUND::RepositoryRoot.readItem_NI::GP::Stream__NI::SOUND::ReadContext_
+
+Headers (20 bytes)
+u64
+u32
+u32
+u32
+
+> 106 Authorization
+	u64
+	u32
+	u32
+	u32
+	u64
+	u32
+	u32
+	u32
+
+	iVar1 = u32
+	if iVar1 == 1 {
+		stack:+0x18 &= 0xfffffffffffffffe
+	}
+
+u32
+u32
+u32
+u32
+
+## 3 Item
+
+> 101
--- tests/test-item.rs
+++ tests/test-item.rs
@@ -5,12 +5,10 @@ use ni_file::raw_data::NIData;
 
 #[test]
 fn test_kontakt5_repository_root() -> Result<()> {
-    let file = include_bytes!("../data/items/kontakt-5-repository-root");
+    let file = include_bytes!("../data/item-frames/118-Kontakt5-RepositoryRoot.data");
     let mut cursor: Cursor<&[u8]> = Cursor::new(file.as_slice());
 
     let _data: NIData = ni_file::raw_data::read_data(&mut cursor)?;
-
-    // panic!("{:?}", data);
 
     Ok(())
 }
