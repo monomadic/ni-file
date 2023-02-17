ba3822f14490b1669169750e18fdfb3c685959ea
nom <nom@nom.local>
2023-02-17 16:10:57 +0800

    fix: test

--- tests/test-item.rs
+++ tests/test-item.rs
@@ -5,7 +5,7 @@ use ni_file::raw_data::NIData;
 
 #[test]
 fn test_kontakt5_repository_root() -> std::result::Result<(), NIFileError> {
-    let file = include_bytes!("../data/item-frames/118-Kontakt5-RepositoryRoot.data");
+    let file = include_bytes!("../data/item-frame/118-Kontakt5-RepositoryRoot.data");
     let mut cursor: Cursor<&[u8]> = Cursor::new(file.as_slice());
 
     let _data: NIData = ni_file::raw_data::read_data(&mut cursor)?;
