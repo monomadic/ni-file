9c2519914e9d4415ab5ede2802614c275ddca76a
nom <nom@nom.local>
2023-02-21 23:38:07 +0800

    improved example syntax

--- examples/container.rs
+++ examples/container.rs
@@ -2,14 +2,15 @@ use ni_file::{repository::Repository, NIFileType};
 
 pub fn main() {
     // lets read a Kontakt 7 file.
-    let file = include_bytes!("../tests/data/files/kontakt-7/000-default.nki");
+    let file = include_bytes!("../tests/data/files/kontakt-7/000-default.nki").as_slice();
 
     // make sure this is a valid repository
     if NIFileType::detect(file) == NIFileType::Repository {
-        let repo = Repository::read(file.as_slice()).unwrap();
-        println!(
-            "major version: {}",
-            repo.root_item().unwrap().major_version()
-        );
+        // read the repository
+        let repo = Repository::read(file).unwrap();
+        // grab the root item
+        let root = repo.root_item().unwrap();
+        // print the major version number
+        println!("major version: {}", root.major_version());
     }
 }
